5716ff3ed36cdeffa6a65fc59223aa94a5a3b687
Max Gordon <gordonmaxc@gmail.com>
2015-01-30 10:44:35 -0800

    hot fix

--- app/assets/javascripts/markers.js
+++ app/assets/javascripts/markers.js
@@ -1,14 +1,14 @@
-var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
-var mapOptions = {
-  zoom: 4,
-  center: myLatlng
-}
-var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-var marker = new google.maps.Marker({
-    position: myLatlng,
-    title:"Hello World!"
-});
-
-// To add the marker to the map, call setMap();
-marker.setMap(map);
+// var myLatlng = new google.maps.LatLng(37.7633,-122.4167);
+// var mapOptions = {
+//   zoom: 13,
+//   center: myLatlng
+// };
+// var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+//
+// var marker = new google.maps.Marker({
+//     position: myLatlng,
+//     title:"Hello World!"
+// });
+//
+// // To add the marker to the map, call setMap();
+// marker.setMap(map);
