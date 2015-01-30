cb74a1b34d812af9c6ce62daf49d364dd5f74a49
Max Gordon <gordonmaxc@gmail.com>
2015-01-30 10:58:25 -0800

    added marker

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,11 +1,23 @@
 $('document').ready(function () {
-  function initialize() {
-    var mapOptions = {
-      center: { lat: 37.7633, lng: -122.4167},
-      zoom: 12
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
-  }
-  google.maps.event.addDomListener(window, 'load', initialize);
+  var map = initializeMap();
+
+  var coords = [37.787791,-122.396917];
+  setMarker(coords, map);
 });
+
+function initializeMap() {
+  var mapOptions = {
+    center: { lat: 37.7633, lng: -122.4167},
+    zoom: 12
+  };
+  return new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+
+function setMarker(coords, map) {
+  var markerLocale = new google.maps.LatLng(37.787791,-122.396917);
+  var marker = new google.maps.Marker({
+      position: markerLocale,
+      map: map,
+      title:"Hello World!"
+  });
+}
--- app/assets/javascripts/markers.js
+++ app/assets/javascripts/markers.js
@@ -1,14 +1,1 @@
-// var myLatlng = new google.maps.LatLng(37.7633,-122.4167);
-// var mapOptions = {
-//   zoom: 13,
-//   center: myLatlng
-// };
-// var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-//
-// var marker = new google.maps.Marker({
-//     position: myLatlng,
-//     title:"Hello World!"
-// });
-//
-// // To add the marker to the map, call setMap();
-// marker.setMap(map);
+
