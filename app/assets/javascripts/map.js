77b46bcded8fb1f0a79ab30587b7bfeeab12ad62
Max Gordon <gordonmaxc@gmail.com>
2015-01-30 11:18:02 -0800

    pin window

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,19 +1,54 @@
 $('document').ready(function () {
   var map = initializeMap({center: { lat: 37.7633, lng: -122.4167}, zoom: 12});
 
+
+
   var coords = [37.787791,-122.396917];
-  setMarker(coords, map);
+  var marker = setMarker("title", coords, map);
+
+
+
+
 });
 
 function initializeMap(mapOptions) {
   return new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
-function setMarker(coords, map) {
-  var markerLocale = new google.maps.LatLng(37.787791,-122.396917);
+function setMarker(title, coords, map) {
+  var markerLocale = new google.maps.LatLng(coords[0],coords[1]);
   var marker = new google.maps.Marker({
       position: markerLocale,
       map: map,
-      title:"Hello World!"
+      title: title
+  });
+
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
   });
 }
