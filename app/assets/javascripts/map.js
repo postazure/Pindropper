59d59f55b366ef690943f6afd4570b77733f206d
Max Gordon <gordonmaxc@gmail.com>
2015-01-30 11:01:32 -0800

    functionize

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,15 +1,11 @@
 $('document').ready(function () {
-  var map = initializeMap();
+  var map = initializeMap({center: { lat: 37.7633, lng: -122.4167}, zoom: 12});
 
   var coords = [37.787791,-122.396917];
   setMarker(coords, map);
 });
 
-function initializeMap() {
-  var mapOptions = {
-    center: { lat: 37.7633, lng: -122.4167},
-    zoom: 12
-  };
+function initializeMap(mapOptions) {
   return new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
