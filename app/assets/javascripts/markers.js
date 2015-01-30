3334209455fa4c0f097294ddac407358fa101c60
Max Gordon <gordonmaxc@gmail.com>
2015-01-30 11:50:11 -0800

    click events

--- app/assets/javascripts/markers.js
+++ app/assets/javascripts/markers.js
@@ -1,1 +1,15 @@
+function setMarker(title, coords, map) {
+  var markerLocale = new google.maps.LatLng(coords[0],coords[1]);
+  var marker = new google.maps.Marker({
+      position: markerLocale,
+      map: map,
+      title: title
+  });
 
+  var contentString = "html here";
+
+  var infowindow = new google.maps.InfoWindow({ content: contentString });
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+}
